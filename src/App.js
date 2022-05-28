ee4b497aff836aefa8ec08c7f46efe69ab2c93af
PamirKalo <kbm2@mail.bg>
2022-05-28 15:39:28 +0300

    Update src/App.js , Add main routes, fetch and dispatch fuctionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import {load} from './redux-store/actions';
+import Header from './components/Header';
+import List from './components/List';
+import Details from './components/Deatails';
+import fake from './assets/fake-data.json';
+const API_URL = 'https://api.jsonbin.io/b/6231abada703bb67492d2b8f';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            // const data = await fetch(API_URL);
+            // const json = await data.json();
+
+            // Add fake data, because of exhausted API
+            const json = fake;
+            dispatch(load(json));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <BrowserRouter>
+            <Header />
+            <div className='App'>Devocean-task</div>
+            <Routes>
+                <Route path='/' element={<List />} />
+                <Route path='/details/:id' element={<Details />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
