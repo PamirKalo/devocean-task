732ef61dce34dbd9d68ab8df4b23f206e20470c0
PamirKalo <kbm2@mail.bg>
2022-05-30 00:42:25 +0300

    Update List.js , Update routes App.js components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { load } from './redux-store/actions';
+import { load_start } from './redux-store/actions';
 
-import {load} from './redux-store/actions';
 import Header from './components/Header';
 import List from './components/List';
-import Details from './components/Deatails';
+import DetailsPage from './components/DetailsPage';
+// import FetchData from './fetch';
 import fake from './assets/fake-data.json';
-const API_URL = 'https://api.jsonbin.io/b/6231abada703bb67492d2b8f';
 
 function App() {
     const dispatch = useDispatch();
+    const fetchData = async () => {
+        // const API_URL = 'https://api.jsonbin.io/b/6231abada703bb67492d2b8f';
+        const test_api = 'https://jsonplaceholder.typicode.com/users'; // free test api
+        dispatch(load_start());
+        try {
+            // const data = await fetch(API_URL);
+            // const data = await fetch(test_api);
+            // const json = await data.json();
+            setTimeout(() => {
+                dispatch(load(fake));
+            }, 1000);
+
+            // dispatch(load(json));
+        } catch (error) {
+            return error;
+        }
+    };
     useEffect(() => {
         fetchData();
     }, []);
 
-    const fetchData = async () => {
-        try {
-            // const data = await fetch(API_URL);
-            // const json = await data.json();
-
-            // Add fake data, because of exhausted API
-            const json = fake;
-            dispatch(load(json));
-        } catch (error) {
-            console.log(error);
-        }
-    };
     return (
         <BrowserRouter>
             <Header />
             <div className='App'>Devocean-task</div>
-            <Routes>
-                <Route path='/' element={<List />} />
-                <Route path='/details/:id' element={<Details />} />
-            </Routes>
+            <main>
+                <Routes>
+                    <Route path='/' element={<List />} />
+                    <Route path='/details/:id' element={<DetailsPage />} />
+                </Routes>
+            </main>
         </BrowserRouter>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,32 @@
+import React, { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import ListRow from './ListRow';
+
 const List = () => {
-    return <div>'BusinessDirectory'</div>;
+    const [catalog, loading] = useSelector((store) => [
+        store.businesses.catalog,
+        store.businesses.loading,
+    ]);
+
+    const rows = catalog.map((el) => {
+        return (
+            <ListRow
+                key={el.id}
+                name={el.name}
+                description={el.description}
+                id={el.id}
+            />
+        );
+    });
+
+    return (
+        <Fragment>
+            <section>
+                <h3>'BusinessDirectory'</h3>
+                {loading === true ? 'Loading...' : rows}
+            </section>
+        </Fragment>
+    );
 };
+
 export default List;
