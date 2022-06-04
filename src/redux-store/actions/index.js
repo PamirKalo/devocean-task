b2d527555e1c739f16e1a474d7e1302ffef4f853
PamirKalo <kbm2@mail.bg>
2022-06-04 13:23:22 +0300

    Create load_failed action and reducer

--- src/redux-store/actions/index.js
+++ src/redux-store/actions/index.js
@@ -1,12 +1,18 @@
 export const load_start = ()=>{
     return {
         type: 'LOAD_START',
-        payload: true,
     }
 }
 export const load = (arr)=>{
     return {
         type: 'LOAD',
         payload: arr,
+    }
+}
+
+export const load_failed = (error)=>{
+    return {
+        type: 'LOAD_FAILED',
+        payload: error,
     }
 }
--- src/redux-store/reducers/index.js
+++ src/redux-store/reducers/index.js
@@ -3,14 +3,17 @@ import { combineReducers } from 'redux';
 const INITIAL = {
     catalog: [],
     loading: false,
+    failed: false,
 };
 
 const businessesDataReducer = (state = INITIAL, action) => {
     switch (action.type) {
         case 'LOAD_START':
-            return { ...state, loading: action.payload };
+            return { ...state, loading: false };
         case 'LOAD':
             return { ...state, catalog: action.payload, loading: false };
+        case 'LOAD_FAILED':
+            return { ...state, loading: false, failed: true };
         default:
             return state;
     }
