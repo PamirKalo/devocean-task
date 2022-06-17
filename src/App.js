05c8d422009eb9a0f5008fe7e83767d4c21e7865
PamirKalo <kbm2@mail.bg>
2022-06-17 10:27:37 +0300

    Add NotFound.js component, refactor fetch request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { load } from './redux-store/actions';
-import { load_start } from './redux-store/actions';
-import { load_failed } from './redux-store/actions';
+import { load, load_start, load_failed } from './redux-store/actions';
 
 import Header from './components/Header';
 import List from './components/List';
@@ -11,31 +9,32 @@ import DetailsPage from './components/DetailsPage';
 import ErrorPage from './components/ErrorPage';
 import './App.css';
 import fake from './assets/fake-data.json';
-const API_URL = 'https://api.jsonbin.io/b/6231abada703bb67492d2b8f';
+import NotFound from './components/NotFound';
+const API_URL = 'https://api.jsonbin.io/b/6231abada703bb67492d2b8f'; // devocean url
+// const API_URL = 'https://jsonplaceholder.typicode.com/comments'; // Free fake API
 
 function App() {
     const dispatch = useDispatch();
-    const fetchData = async () => {
-        dispatch(load_start());
-        try {
-            const data = await fetch(API_URL);
-            const json = await data.json();
-
-            if (json.success === false) {
-                // dispatch(load_failed(json));
-
-                // ****** "API Requests Quota is Exhausted. ******
-                // Purchase more requests at https://jsonbin.io/pricing."
-                // Add fake data
-                dispatch(load(fake));
-            } else dispatch(load(json));
-        } catch (error) {
-            dispatch(load_failed(error));
-            console.log('error in app.js->>>>>', error);
-        }
-    };
     useEffect(() => {
-        fetchData();
+        (async function (url) {
+            dispatch(load_start());
+            try {
+                const data = await fetch(url);
+                const json = await data.json();
+
+                if (json.success === false) {
+                    dispatch(load_failed(json));
+
+                    // ****** "API Requests Quota is Exhausted. ******
+                    // Purchase more requests at https://jsonbin.io/pricing."
+                    // Add fake data
+                    dispatch(load(fake));
+                } else dispatch(load(json));
+            } catch (error) {
+                dispatch(load_failed(error));
+                console.log('fetch api error ->>>>>', error);
+            }
+        })(API_URL);
     }, []);
 
     return (
@@ -43,9 +42,12 @@ function App() {
             <Header />
             <main>
                 <Routes>
-                    <Route path='/' element={<List />} />
-                    <Route path='/details/:id' element={<DetailsPage />} />
-                    <Route path='*' element={<ErrorPage />} />
+                    <Route path='/'>
+                        <Route index element={<List />} />
+                        <Route path='details/:id' element={<DetailsPage />} />
+                        <Route path='error_page' element={<ErrorPage />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Route>
                 </Routes>
             </main>
         </BrowserRouter>
